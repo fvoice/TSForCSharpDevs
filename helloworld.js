8a82aa5df53bac07cf49018d60b98ae919b3e6e9
fvoice <fvoice@yandex.ru>
2020-12-14 22:40:50 +0100

    Initial commit

---
+++ helloworld.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.HelloWorld = void 0;
+var HelloWorld = /** @class */ (function () {
+    function HelloWorld() {
+    }
+    HelloWorld.prototype.SayHi = function () {
+        console.log("hello world");
+    };
+    return HelloWorld;
+}());
+exports.HelloWorld = HelloWorld;
+var helloWorld = new HelloWorld();
+helloWorld.SayHi();
+// node helloWorld
+// output: hello world
+//# sourceMappingURL=helloworld.js.map
---
+++ helloworld.ts
@@ -0,0 +1,12 @@
+export class HelloWorld{
+    SayHi() {
+       console.log("hello world");
+    }
+}
+
+var helloWorld = new HelloWorld();
+helloWorld.SayHi();
+
+// node helloWorld
+// output: hello world
+  
